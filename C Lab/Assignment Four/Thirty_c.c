d9b9ce9819837caec4afbbee0d6c18aa91abf4cb
Himangshu <himangs503@gmail.com>
2024-04-25 09:52:13 +0530

    Assignment Four modify

--- C Lab/Assignment Four/Thirty_c.c
+++ C Lab/Assignment Four/Thirty_c.c
@@ -8,16 +8,16 @@ int main ()
     scanf("%d",&n);
     //Print pattern here
     col = 0;
-    midcol = (2*n-1)/2+1;
+    midrow = (2*n-1)/2+1;
     for (int i = 1; i <= 2*n-1;i++)
     {
-        if(i <= midcol) col++;
+        if(i <= midrow) col++;
         else col--;
         num = 0;
-        midrow  = (2*col-1)/2+1;
+        midcol  = (2*col-1)/2+1;
         for (int j = 1; j <= 2*col-1; j++)
         {
-            if (j <= midrow) num++;
+            if (j <= midcol) num++;
             else num--;
             printf("%d",num);
             
